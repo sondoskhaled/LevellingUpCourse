8b4d5429eda7f0d34f87f17b2800530f10b4ca74
sondos <sondoskhaled22@gmail.com>
2024-08-17 11:38:55 +0300

    Open Chrome browser and go to google.com

--- src/test/java/testPackage/TestBase.java
+++ src/test/java/testPackage/TestBase.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class OpenBrowser {
+public class TestBase {
     @Test
     public void firstTestMethod()
     {

7832cca89f371c510035bb338f160579341eefec
sondos <sondoskhaled22@gmail.com>
2024-08-17 23:39:00 +0300

    Search and assert the text of the first result

--- src/test/java/testPackage/TestBase.java
+++ src/test/java/testPackage/TestBase.java
@@ -1,10 +1,16 @@
 package testPackage;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import static org.testng.Assert.assertTrue;
 
 public class TestBase {
@@ -31,6 +37,25 @@ public class TestBase {
         driver.get("https://www.google.com/ncr");
         boolean isLogoVisible = driver.findElement(By.className("lnXdpd")).isDisplayed();
         assertTrue(isLogoVisible);
+    }
+
+    @Test
+    public void verifySearchResult(){
+        driver.get("https://www.google.com/ncr");
+        WebElement searchField = driver.findElement(By.id("APjFqb"));
+        searchField.sendKeys("Selenium WebDriver");
+        searchField.sendKeys(Keys.ENTER);
+        // Wait until the page is fully loaded
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        });
+        List<WebElement> searchResult =driver.findElements(By.xpath("//div[@id='search']//div[last()]//h3"));
+        String firstResult = searchResult.getFirst().getText();
+        Assert.assertEquals(firstResult , "Selenium - Web Browser Automation");
+
     }
     @AfterClass
     public void tearDown(){

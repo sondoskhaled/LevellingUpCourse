0d74bce0ff6cd05f2895379819ef9a54785dd42b
sondos <sondoskhaled22@gmail.com>
2024-08-17 14:27:02 +0300

    Add BeforeClass and AfterClass test methods

--- src/test/java/testPackage/TestBase.java
+++ src/test/java/testPackage/TestBase.java
@@ -4,35 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import static org.testng.Assert.assertTrue;
 
 public class TestBase {
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        this.driver = new ChromeDriver();
+    }
     @Test
     public void goToGoogle()
     {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        driver.quit();
     }
     @Test
     public void verifyTabTitle()
     {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         String tabTitle = driver.getTitle();
         Assert.assertEquals(tabTitle,"Google");
-        driver.quit();
-
     }
     @Test
     public void verifyGoogleLogo()
     {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         boolean isLogoVisible = driver.findElement(By.className("lnXdpd")).isDisplayed();
         assertTrue(isLogoVisible);
+    }
+    @AfterClass
+    public void tearDown(){
         driver.quit();
-
     }
 }

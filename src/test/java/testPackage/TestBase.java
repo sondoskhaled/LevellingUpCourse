6077579a36ee8ae43f65565f080bfd0669397174
sondos <sondoskhaled22@gmail.com>
2024-08-17 12:05:08 +0300

    Assert that the Google logo is displayed

--- src/test/java/testPackage/TestBase.java
+++ src/test/java/testPackage/TestBase.java
@@ -1,15 +1,27 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
 
 public class TestBase {
     @Test
-    public void firstTestMethod()
+    public void goToGoogle()
     {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
         driver.quit();
+    }
+    @Test
+    public void verifyGoogleLogo()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        boolean isLogoVisible = driver.findElement(By.className("lnXdpd")).isDisplayed();
+        assertTrue(isLogoVisible);
+        driver.quit();
+
     }
 }

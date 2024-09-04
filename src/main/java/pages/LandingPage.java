5bc1a091a903451e05b521fc45059b56b3784161
sondos <sondoskhaled22@gmail.com>
2024-09-04 18:38:20 +0300

    Task 2 verify logo is displayed

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -1,18 +1,37 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class LandingPage {
     private final WebDriver driver;
+    Wait<WebDriver> wait;
     private final By searchField = By.xpath("//input[@id = 'searchbox_input']");
+    private final By logo = By.xpath("(//a/img)[1]");
     public LandingPage (WebDriver driver){
         this.driver=driver;
-    }
+        this.wait =
+                new FluentWait<>(driver)
+                        .withTimeout(Duration.ofSeconds(60))
+                        .pollingEvery(Duration.ofMillis(100))
+                        .ignoring(NotFoundException.class)
+                        .ignoring(StaleElementReferenceException.class)
+                        .ignoring(AssertionError.class);    }
 
     public void navigate(){
         driver.navigate().to("https://duckduckgo.com/");
+    }
+
+    public void logoIsDisplayed(){
+        wait.until(d-> {
+        Assert.assertTrue(driver.findElement(logo).isDisplayed());
+        return true;});
     }
 
     public void search (String query){
---
+++ src/test/java/testPackage/modular/pom/Task2Tests.java
@@ -0,0 +1,35 @@
+package testPackage.modular.pom;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LandingPage;
+import java.time.Duration;
+
+public class Task2Tests {
+    LandingPage landingPage;
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp (){
+        this.driver = new ChromeDriver();
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(1080 , 720));
+        landingPage = new LandingPage(driver);
+        landingPage.navigate();
+    }
+
+    @Test(description = "verify that the logo is displayed")
+    public void verifyLogoIsDisplayed(){
+            landingPage.logoIsDisplayed();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
